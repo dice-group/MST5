b7b3faf44fdd4f376fe1fe697761b5f54be9548b
Mengshi Ma <makevin23@gmail.com>
2023-04-24 14:46:35 +0200

    update file path

--- run_qald_tests.sh
+++ run_qald_tests.sh
@@ -9,5 +9,5 @@ languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "hy" "uk" "es")
 for lang in "${languages[@]}"
 do
     echo "Generating predicted qald file for ${lang}"
-    python process_functions/test_and_build_qald.py --model fine-tuned_models/${model} -t datasets/qald_9_plus/qald_9_pp_test_wikidata_linguistic.json -o pred_files/${model}/${lang}.json -l ${lang} ${ling_context}
+    python code/test_and_build_qald.py --model fine-tuned_models/${model} -t datasets/qald_9_plus/qald_9_pp_test_wikidata_linguistic.json -o pred_files/${model}/${lang}.json -l ${lang} ${ling_context}
 done
