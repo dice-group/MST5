6d39dd7bd24f9d247c2b47a4faa7f9e8db540db0
Mengshi Ma <makevin23@gmail.com>
2023-04-25 14:12:31 +0200

    add export message

--- code/utils/export.py
+++ code/utils/export.py
@@ -6,9 +6,12 @@ def export_csv(output_file, dataset):
         writer = csv.writer(f)
         writer.writerows(dataset)
     f.close()
+    print("csv file is exported to ", output_file)
+    
 
 
 def export_json(output_file, dataset):
     with open(output_file, 'w') as f:
         json.dump(dataset, f)
-    print("json file saved to ", output_file)
+    f.close()
+    print("json file is exported to ", output_file)
