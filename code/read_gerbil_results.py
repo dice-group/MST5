5162a1c750b250b7b679021f0d329756b05ca724
Mengshi Ma <makevin23@gmail.com>
2023-04-25 14:15:05 +0200

    add export csv results

--- code/read_gerbil_results.py
+++ code/read_gerbil_results.py
@@ -1,26 +1,35 @@
 import urllib.request
-import pandas
+import pandas as pd
 import argparse
-import csv
 
-def get_html_data():
-    fp = urllib.request.urlopen("https://gerbil-qa.aksw.org/gerbil/experiment?id=202304170000")
+
+def get_html_data(id):
+    fp = urllib.request.urlopen(
+        "https://gerbil-qa.aksw.org/gerbil/experiment?id=" + id)
     mybytes = fp.read()
     mystr = mybytes.decode("utf8")
     fp.close()
     return mystr
 
-gerbil_html = get_html_data()
 
-def get_results(html):
-    html = pandas.read_html(html)[0].rename(columns={
-    "Unnamed: 3": "Benchmark"
-})
+def get_results(html, output_file):
+    html = pd.read_html(html)[0].rename(columns={
+        "Unnamed: 3": "Benchmark"
+    })
     html = html[html['Benchmark'].isna()]
-    return html.drop(columns=['Dataset', 'Language', 'Error Count', 'avg millis/doc', 'Timestamp', 'GERBIL version', 'Benchmark'])
+    result = html.drop(
+        columns=[
+            'Dataset',
+            'Language',
+            'Error Count',
+            'avg millis/doc',
+            'Timestamp',
+            'GERBIL version',
+            'Benchmark'
+        ]
+    )
+    result.to_csv(output_file, index=False)
 
-result_df = get_results(gerbil_html)
-print(result_df)
 
 def main():
     # create an ArgumentParser object
@@ -28,17 +37,18 @@ def main():
         description="A program to extract lcqald questions and queries to csv dataset")
 
     # add arguments to the parser
-    parser.add_argument("knowledge_graph", choices=[
-                        "wikidata", "dbpedia"], help="choose the knowledge graph", default="wikidata")
-    parser.add_argument("-i", "--input", type=str, help="name of input file")
+    parser.add_argument("--id", type=str,
+                        help="id of GERBIL experiment")
     parser.add_argument("-o", "--output", type=str,
-                        help="name of output file", default="lcqald.csv")
+                        help="name of output file")
 
     # parse the arguments
     args = parser.parse_args()
+    gerbil_html = get_html_data(args.id)
+    get_results(gerbil_html, args.output)
+
 
 # check if this module is the main program
 if __name__ == "__main__":
     # call the main function
     main()
-
