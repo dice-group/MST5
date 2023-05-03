f732fe2b4a7412cfe2b50eb138c1199b54c78ab0
Mengshi Ma <makevin23@gmail.com>
2023-05-03 11:06:55 +0200

    add main function

--- code/gerbil_eval.py
+++ code/gerbil_eval.py
@@ -1,32 +1,46 @@
 """GERBIL Evaluation for QA systems."""
 from utils.gerbil import *
 import os
-
-folder_path = "pred_files/mt5/4_mt5/"
-
-languages = [f.split(".")[0] for f in os.listdir(folder_path) if f.endswith('.json')]
+import argparse
 
 gold_name = 'qald 9 plus test'
 gold_file_path = "datasets/qald9plus/qald_9_pp_test_wikidata.json"
 
-exp_setting = "four-shot mt5 "
-pred_pfad_prefix = "pred_files/mt5/4_mt5/"
-output_file = pred_pfad_prefix + "results.csv"
+def main():
+    parser = argparse.ArgumentParser(
+        description="A script to run gerbil experiments")
 
+    # add arguments to the parser
+    parser.add_argument("--exp_setting", type=str,
+                        help="experiment setting", required=True)
+    parser.add_argument("--pred_pfad", type=str,
+                        help="pfad of prediction files", required=True)
 
-upload_gold(gold_name, gold_file_path)
-upload_pred(exp_setting, pred_pfad_prefix, languages)
+    args = parser.parse_args()
 
-# set experiment data
-gold = {gold_name: gold_file_path.split('/')[-1]}
+    exp_setting = args.exp_setting + " "
+    pred_pfad = args.pred_pfad
+    output_file = pred_pfad + "results.csv"
 
-pred = dict()
-for lang in languages:
-    pred[exp_setting + lang] = lang + '.json'
+    languages = [f.split(".")[0] for f in os.listdir(pred_pfad) if f.endswith('.json')]
 
-experiment_id = run_experiment(gold, pred).text
-gerbil_html = get_html_data(experiment_id)
-if gerbil_html is not None:
-    get_results(gerbil_html, output_file)
-else: 
-    print("Error when getting GERBIL results")
+    upload_gold(gold_name, gold_file_path)
+    upload_pred(exp_setting, pred_pfad, languages)
+
+    # set experiment data
+    gold = {gold_name: gold_file_path.split('/')[-1]}
+
+    pred = dict()
+    for lang in languages:
+        pred[exp_setting + lang] = lang + '.json'
+
+    experiment_id = run_experiment(gold, pred).text
+    gerbil_html = get_html_data(experiment_id)
+    if gerbil_html is not None:
+        get_results(gerbil_html, output_file)
+    else: 
+        print("Error when getting GERBIL results")
+
+if __name__ == "__main__":
+    # call the main function
+    main()
