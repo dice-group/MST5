c9ad3faf4c9d29b10ac18c7ae18ea6d2dca2b9f6
Mengshi Ma <makevin23@gmail.com>
2023-05-10 16:26:29 +0200

    adapt to fine-tune one-shot mt5-xl

--- run_summarization_ds.sh
+++ run_summarization_ds.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 dataset_name="11_linguistic"
-output_dir="fine-tuned_models/${dataset_name}_mt5-xxl"
+output_dir="fine-tuned_models/${dataset_name}_mt5-xl"
 
 deepspeed run_summarization.py \
     --deepspeed deepspeed/ds_config_zero3.json \
-    --model_name_or_path "google/mt5-xxl" \
+    --model_name_or_path "google/mt5-xl" \
     --do_train \
-    --train_file datasets/qald_9_plus/${dataset_name}/q9pp_train.csv \
+    --train_file datasets/${dataset_name}/q9pp_train.csv \
     --output_dir ${output_dir} \
     --num_train_epochs 100 \
     --per_device_train_batch_size=4 \
     --overwrite_output_dir \
     --save_steps 30000 \
-    --save_total_limit 3 \
+    --save_total_limit 1 \
     --report_to wandb \
-    --tf32 0 \
-    --fp16 1 \
+    --tf32 1 \
+    --fp16 0 \
     --gradient_checkpointing 1 \
     --gradient_accumulation_steps 4
 
