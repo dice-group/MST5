2b0ccd6387ae4cacc3f2f1fa56bf1bfb68b8b0c7
Mengshi Ma <makevin23@gmail.com>
2023-05-12 17:18:22 +0200

    eval script for exp5

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-model="1_mt5-xl"
-exp_setting="1-shot_mt5-xl"
+model="11_linguistic_mt5-xl"
+exp_setting="all_lang_linguistic_mt5-xl"
 pred_pfad=pred_files/${model}/
 linguitic_context="True"
 
@@ -14,7 +14,7 @@ do
     echo "Generating predicted qald file for ${lang}"
     python code/test_and_build_qald.py \
         --model fine-tuned_models/${model} \
-        -t datasets/qald9plus/qald_9_pp_test_wikidata.json \
+        -t datasets/qald9plus/qald_9_pp_test_wikidata_linguistic.json \
         -o pred_files/${model}/${lang}.json \
         -l ${lang} \
         --linguistic_context ${linguitic_context}
