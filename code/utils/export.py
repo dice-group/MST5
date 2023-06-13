d223d323fef57f7f731ccc81c9366a207bafacce
Mengshi Ma <makevin23@gmail.com>
2023-06-13 13:33:58 +0200

    refactor

--- code/utils/export.py
+++ code/utils/export.py
@@ -3,20 +3,20 @@ import json
 import os
 
 def export_csv(output_file, dataset):
-    dir_path = os.path.dirname(output_file)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    check_and_makedir_if_not_exist(output_file)
     with open(output_file, "w") as f:
         writer = csv.writer(f)
         writer.writerows(dataset)
     f.close()
     print("csv file is exported to ", output_file)
     
-
 def export_json(output_file, dataset):
-    dir_path = os.path.dirname(output_file)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    check_and_makedir_if_not_exist(output_file)
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(dataset, f, ensure_ascii=False)
     print("json file is exported to ", output_file)
+
+def check_and_makedir_if_not_exist(output_file):
+    dir_path = os.path.dirname(output_file)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
