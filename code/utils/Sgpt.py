19f7d0881060b9bfa7221224fe9cb6b9879a00cf
Mengshi Ma <makevin23@gmail.com>
2023-06-19 14:34:27 +0200

    add change sparql functions

--- code/utils/Sgpt.py
+++ code/utils/Sgpt.py
@@ -1,6 +1,7 @@
 from utils.query import ask_dbpedia
 from utils.qald import build_qald_entry
 from utils.data_io import export_json
+from utils.process_query import *
 
 prefixes = [
     "PREFIX dbo: <http://dbpedia.org/ontology/>"
@@ -61,6 +62,7 @@ class Sgpt:
             self.pred_qald.append(build_qald_entry(
                 id, "example question", entry.pred_query, answer, "en"))
 
+
 class Sgpt_entry:
     def __init__(self, ref_query: str, pred_query: str) -> None:
         self.ref_query: str = ref_query
@@ -68,7 +70,42 @@ class Sgpt_entry:
 
     def add_prefixes(self, query: str) -> str:
         return (' ').join(prefixes) + query
-    
+
     def get_answer(self, query: str) -> dict:
         query_with_prefixes = self.add_prefixes(query)
         return ask_dbpedia(query_with_prefixes)
+
+
+def build_sgpt_dict(sgpt_set):
+    sgpt_dict = dict()
+    for entry in sgpt_set:
+        sgpt_dict[entry["en_ques"]] = entry
+    return sgpt_dict
+
+
+def is_question_in_sgpt(question_string, sgpt_set):
+    return question_string in sgpt_set
+
+
+def export_sgpt_file(output, sgpt_set):
+    sgpt_train = [v for v in sgpt_set.values()]
+    export_json(output, sgpt_train)
+
+
+def change_sparql(sgpt_dict, qald_entry, question_string):
+    fil_sparql = postprocess_sparql(
+        preprocess_sparql(qald_entry["query"]["sparql"]))
+    sgpt_dict[question_string]["fil_sparql"] = fil_sparql
+
+
+def change_dbpedia_sparql_to_sparql(wikidata_qald, sgpt_dbpedia, output):
+    sgpt_dict = build_sgpt_dict(sgpt_dbpedia)
+    qald_entries = wikidata_qald["questions"]
+    for qald_entry_wikidata in qald_entries:
+        for question in qald_entry_wikidata["question"]:
+            if question["language"] == "en":
+                question_string = question["string"].lower()
+                if is_question_in_sgpt(question_string, sgpt_dict):
+                    change_sparql(sgpt_dict, qald_entry_wikidata,
+                                  question_string)
+    export_sgpt_file(output, sgpt_dict)
