ae07aac3e9c1e86fbb34eab4877f4816fb3afbee
Mengshi Ma <makevin23@gmail.com>
2023-06-19 10:16:56 +0200

    script for fine-tune

--- train_ds.sh
+++ train_ds.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-dataset_name=""
-model_name="google/mt5-xl"
+dataset_name="q9pp-ling-context"
+model_name="fine-tuned_models/LC-QuAD"
 output_dir="fine-tuned_models/${dataset_name}"
 run_name="${model_name}-${dataset_name}"
-train_file=""
-validation_file=""
+train_file="datasets/q9pp_ling_context.csv"
 
-deepspeed --num_gpus=1 code/train_new.py \
+deepspeed --include=localhost:1 --master_port 60000 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
     --model_name_or_path ${model_name} \
     --do_train \
-    --do_eval \
-    --eval_steps 1500 \
     --train_file ${train_file} \
-    --validation_file ${validation_file} \
     --output_dir ${output_dir} \
     --num_train_epochs 15 \
-    --per_device_train_batch_size=4 \
+    --per_device_train_batch_size=16 \
     --overwrite_output_dir \
-    --save_steps 3000 \
+    --save_steps 500 \
     --save_total_limit 2 \
     --report_to wandb \
     --run_name ${run_name}\
