b1eec6afe22370f8ad62571374c68e972aeec8b5
Mengshi Ma <makevin23@gmail.com>
2023-06-20 10:03:20 +0200

    wip: lcquad

---
+++ code/utils/LCquad.py
@@ -0,0 +1,13 @@
+class LCquad:
+    def __init__(self) -> None:
+        pass
+
+    def export_train_csv(self, include_linguistic_context: bool=False, include_entity_knowledge: bool=False):
+        pass
+
+class LCquad_entry:
+    def __init__(self) -> None:
+        pass
+
+    def get_entity_knowledge(self):
+        pass
