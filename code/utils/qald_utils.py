1c6f94b33d2b859def6abed0cb761ab00632f1d5
Mengshi Ma <makevin23@gmail.com>
2023-06-20 11:40:21 +0200

    refactor

--- code/utils/qald_utils.py
+++ code/utils/qald_utils.py
@@ -1,168 +1,63 @@
-from utils.data_io import *
+from typing import Dict
 from utils.process_query import preprocess_sparql
-from utils.linguistic_parser import get_linguistic_context
-import spacy
-import re
-
-nlp_en = spacy.load("en_core_web_sm")
-nlp_zh = spacy.load("zh_core_web_sm")
-nlp_de = spacy.load("de_core_news_sm")
-nlp_fr = spacy.load("fr_core_news_sm")
-nlp_ja = spacy.load("ja_core_news_sm")
-nlp_lt = spacy.load("lt_core_news_sm")
-nlp_es = spacy.load("es_core_news_sm")
-nlp_ru = spacy.load("ru_core_news_sm")
-nlp_uk = spacy.load("uk_core_news_sm")
-nlp_es = spacy.load("es_core_news_sm")
-
-nlp_dict = {
-    "en": nlp_en,
-    "zh": nlp_zh,
-    "de": nlp_de,
-    "fr": nlp_fr,
-    "ja": nlp_ja,
-    "lt": nlp_lt,
-    "es": nlp_es,
-    "uk": nlp_uk,
-    "ru": nlp_ru,
-    "ba": nlp_ru,
-    "be": nlp_ru
-}
 
 
-class Qald:
-    def __init__(self, qald_file: dict) -> None:
-        self.qald = self.init_qald_list(qald_file)
+def build_qald_entry(id, question_string, sparql_query, answer, language):
+    entry_id = {"id": id}
 
-    def init_qald_list(self, qald_dataset: dict) -> list:
-        qald_list = []
-        if qald_dataset:
-            dataset = qald_dataset["questions"]
-            for entry in dataset:
-                qald_entry = Qald_entry(entry["id"], entry["question"], entry["query"]["sparql"], entry["answers"])
-                qald_list.append(qald_entry)
-        return qald_list
-    
-    def add_entry(self, id, language, question_string, query, answers):
-        question = [{
-            "language": language,
-            "string": question_string
-        }]
-        self.qald.append(Qald_entry(id, question, query, answers))
+    entry_question = {"question": [
+        {"language": language, "string": question_string}]}
 
-    def export_train_csv(self, output_file, languages, include_linguistic_context=False, include_entity_knowledge=False) -> None:
-        csv_dataset = [["question", "query"]]
-        qald_entry: Qald_entry
-        for qald_entry in self.qald:
-            query = qald_entry.preprocessed_query
-            for language in languages:
-                if language in qald_entry.questions:
-                    question = qald_entry.get_question_string_by_language(language, include_linguistic_context, include_entity_knowledge)
-                    csv_dataset.append([question, query])
-        export_csv(output_file, csv_dataset)
-    
+    entry_query = {"query": {"sparql": sparql_query}}
 
-    def export_qald_json(self, languages: list, output: str, include_linguistic_context: bool = False, include_entity_knowledge: bool = False) -> None:
-        qald_entries: list = []
-        qald_entry: Qald_entry
-        for qald_entry in self.qald:
-            qald_entries.append(qald_entry.build_qald_format_entry(
-                languages, include_linguistic_context, include_entity_knowledge))
-        export_json(output, {"questions": qald_entries})
+    entry_answers: Dict = {"answers": [answer]}
 
-    def get_question_string_with_id(self, language, include_linguistic_context: bool = False, include_entity_knowledge: bool = False) -> list:
-        questions_with_id = []
-        qald_entry: Qald_entry
-        for qald_entry in self.qald:
-            question_string = qald_entry.get_question_string_by_language(language, include_linguistic_context, include_entity_knowledge)
-            questions_with_id.append([qald_entry.id, question_string])
-        return questions_with_id
+    qald_entry = {
+        "id": entry_id["id"],
+        "question": entry_question["question"],
+        "query": entry_query["query"],
+        "answers": entry_answers["answers"],
+    }
 
-class Qald_entry:
-    def __init__(self, id, questions, query, answers) -> None:
-        self.id = id
-        self.questions: dict = self.get_questions(questions)
-        self.query = query
-        self.preprocessed_query = preprocess_sparql(query)
-        self.answers = answers
-
-    def get_questions(self, questions: list) -> dict:
-        question_strings = {}
-        for q in questions:
-            language = q["language"]
-            question_strings[language] = Question(language, q["string"])
-        return question_strings
-
-    def get_entity_knowledge(self) -> list:
-        pattern = r'\bwd_\w+\b'
-        entities = re.findall(pattern, self.preprocessed_query)
-        return entities
+    return qald_entry
 
 
-    def build_qald_format_entry(self, languages: list, include_linguistic_context: bool, include_entity_knowledge: bool) -> dict:
-        entry_id = {"id": self.id}
-
-        question_lang_and_string = self.get_question_lang_and_strings(
-            languages, include_linguistic_context, include_entity_knowledge)
-
-        entry_question = {
-            "question": question_lang_and_string
-        }
-
-        entry_query = {"query": {"sparql": self.query}}
-
-        entry_answers: dict = {"answers": [self.answers]}
-
-        qald_entry = {
-            "id": entry_id["id"],
-            "question": entry_question["question"],
-            "query": entry_query["query"],
-            "answers": entry_answers["answers"],
-        }
-
-        return qald_entry
-
-    def get_question_lang_and_strings(self, languages, include_linguistic_context: bool, include_entity_knowledge: bool) -> list:
-        question_lang_and_string = []
-        entity_knowledge = []
-        if include_entity_knowledge:
-            entity_knowledge = self.get_entity_knowledge()
-        for language in languages:
-            question_string = self.questions[language].build_question_string(include_linguistic_context, entity_knowledge)
-            question_lang_and_string.append(
-                {
-                    "language": language,
-                    "string": question_string
-                }
-            )
-        return question_lang_and_string
-    
-    def get_question_string_by_language(self, language, include_linguistic_context: bool, include_entity_knowledge: bool) -> str:
-        entity_knowledge = []
-        if include_entity_knowledge:
-            entity_knowledge = self.get_entity_knowledge()
-        return self.questions[language].build_question_string(include_linguistic_context, entity_knowledge)
+def build_question_string_with_linguistic(question):
+    return " ".join(question["doc"]) \
+        + " <pad> " + " ".join(question["pos"]) \
+        + " <pad> " + " ".join(question["dep"]) \
+        + " <pad> " + " ".join(map(str, question["dep_depth"]))
 
 
-class Question:
-    def __init__(self, language, question_string) -> None:
-        self.language = language
-        self.string = question_string
+def get_question_list_with_id(data, languages, linguisitic_context):
+    question_list = []
+    qald_list = data["questions"]
 
-    def add_linguistic_context(self, nlp):
-        self.doc, self.pos, self.dep, self.dep_depth = get_linguistic_context(
-            nlp, self.string)
+    for qald_entry in qald_list:
+        for question in qald_entry["question"]:
+            if question["language"] in languages:
+                if linguisitic_context:
+                    build_question_string_with_linguistic(question)
+                else:
+                    ques_str = question["string"]
+                question_list.append([qald_entry["id"], ques_str])
+                break
+    return question_list
 
-    def add_entity_knowledge(self, entities):
-        self.entities = entities
 
-    def build_question_string(self, include_linguistic_context: bool, entity_knowledge: list):
-        question_string = self.string
-        if include_linguistic_context:
-            self.add_linguistic_context(nlp_dict[self.language])
-            question_string += " <pad> " + " ".join(self.pos) \
-                + " <pad> " + " ".join(self.dep) \
-                + " <pad> " + " ".join(map(str, self.dep_depth))
-        if entity_knowledge:
-            question_string += " <pad> " + " ".join(entity_knowledge)
-        return question_string
+def get_question_query_list(data, languages, linguistic):
+    question_query_list = [['question', 'query']]
+
+    qald_list = data["questions"]
+
+    for qald_entry in qald_list:
+        for lang_ques_dict in qald_entry["question"]:
+            query = preprocess_sparql(qald_entry["query"]["sparql"])
+            if lang_ques_dict["language"] in languages:
+                if linguistic:
+                    ques_str = build_question_string_with_linguistic(
+                        lang_ques_dict)
+                else:
+                    ques_str = lang_ques_dict["string"]
+                question_query_list.append([ques_str, query])
+    return question_query_list
