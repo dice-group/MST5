5b21156e862ce4725a2120e0d073b99510da8796
Mengshi Ma <makevin23@gmail.com>
2023-06-20 12:51:05 +0200

    update parameter

--- train_ds.sh
+++ train_ds.sh
@@ -17,7 +17,7 @@ deepspeed --num_gpus=1 code/train_new.py \
     --validation_file ${validation_file} \
     --output_dir ${output_dir} \
     --num_train_epochs 15 \
-    --per_device_train_batch_size=4 \
+    --per_device_train_batch_size=16 \
     --overwrite_output_dir \
     --save_steps 3000 \
     --save_total_limit 2 \
