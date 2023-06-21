f396e439343106fa13c92153a70e3e9549a7c05e
Mengshi Ma <makevin23@gmail.com>
2023-06-21 10:12:51 +0200

    refactor

--- code/test_and_build_qald.py
+++ code/test_and_build_qald.py
@@ -6,6 +6,21 @@ from utils.Qald import Qald
 from tqdm import tqdm
 
 
+def get_query_and_answer(summarizer, question_string):
+    query_pred = predict_query(summarizer, question_string)
+    sparql_query = postprocess_sparql(query_pred)
+    answer = ask_wikidata(sparql_query)
+    return sparql_query, answer
+
+
+def get_question_list(args, test_file):
+    testset = Qald(test_file)
+    question_list = testset.get_question_string_with_id(
+        args.language, args.linguistic_context, args.entity_knowledge)
+
+    return question_list
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="A program to use model to predict query and build qald dataset")
@@ -26,16 +41,15 @@ def main():
     args = parser.parse_args()
 
     test_file = read_json(args.test)
-    testset = Qald(test_file)
-    summarizer = init_summarizer(args.model)
-    question_list = testset.get_question_string_with_id(args.language, args.linguistic_context, args.entity_knowledge)
+    question_list = get_question_list(args, test_file)
 
+    summarizer = init_summarizer(args.model)
     result = Qald({})
     for id, question_string in tqdm(question_list):
-        query_pred = predict_query(summarizer, question_string)
-        sparql_query = postprocess_sparql(query_pred)
-        answer = ask_wikidata(sparql_query)
-        result.add_entry(id, args.language, question_string, sparql_query, answer)
+        sparql_query, answer = get_query_and_answer(
+            summarizer, question_string)
+        result.add_entry(id, args.language, question_string,
+                         sparql_query, answer)
     result.export_qald_json([args.language], args.output)
 
 
