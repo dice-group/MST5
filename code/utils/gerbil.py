64aee0336bcfe9234bdd4500937f1de25998dde6
Mengshi Ma <makevin23@gmail.com>
2023-06-21 13:23:30 +0200

    fix gerbil

--- code/utils/gerbil.py
+++ code/utils/gerbil.py
@@ -16,39 +16,36 @@ print(f"jsessionid={jsessionid}")
 
 
 UPLOAD_HEADERS = {
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6,ja;q=0.5',
-    'Connection': 'keep-alive',
-    'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryBbipJCvvqkGEBh6H',
-    'Cookie': f'JSESSIONID={jsessionid}',
-    'Origin': 'https://gerbil-qa.aksw.org',
-    'Referer': 'https://gerbil-qa.aksw.org/gerbil/config',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57',
-    'X-Requested-With': 'XMLHttpRequest',
-    'sec-ch-ua': '"Microsoft Edge";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"'
-}
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+        'Cookie': 'JSESSIONID=265042E6F0ECFC6AEEA55C409FB7168F',
+        'Origin': 'https://gerbil-qa.aksw.org',
+        'Referer': 'https://gerbil-qa.aksw.org/gerbil/config',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+    }
 
 SUBMIT_HEADERS = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-    'Connection': 'keep-alive',
-    'Cookie': f'JSESSIONID={jsessionid}',
-    'Referer': 'https://gerbil-qa.aksw.org/gerbil/config',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
-    'X-Requested-With': 'XMLHttpRequest',
-    'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-}
-
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+        'Connection': 'keep-alive',
+        'Cookie': 'JSESSIONID=265042E6F0ECFC6AEEA55C409FB7168F',
+        'Referer': 'https://gerbil-qa.aksw.org/gerbil/config',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+    }
 experiment_url_prefix = "https://gerbil-qa.aksw.org/gerbil/experiment?id="
 execute_url_prefix = "https://gerbil-qa.aksw.org/gerbil/execute?experimentData="
 
