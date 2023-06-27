e0fe68bdf80ab3c2a77b87f0dac6314e565f9488
Mengshi Ma <makevin23@gmail.com>
2023-06-27 13:16:55 +0200

    add training script

--- train.sh
+++ train.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-dataset_name="11_linguistic"
-output_dir="fine-tuned_models/${dataset_name}_mt5"
+run_name="mt5-base-qald9-dbpedia"
+model_name="google/mt5-base"
+output_dir="fine-tuned_models/${run_name}"
+train_file="datasets/qald9plus/dbpedia/qald_9_plus-train-dbpedia.csv"
 
-python train.py \
+CUDA_VISIBLE_DEVICES=1 python train.py \
     --model_name_or_path "google/mt5-base" \
     --do_train \
-    --do_eval \
-    --train_file datasets/qald_9_plus/${dataset_name}/q9pp_train.csv \
-    --validation_file datasets/qald_9_plus/12/q9pp_test.csv \
+    --train_file ${train_file} \
     --output_dir ${output_dir} \
-    --num_train_epochs 300 \
-    --per_device_train_batch_size=4 \
+    --num_train_epochs 100 \
+    --per_device_train_batch_size=8 \
     --overwrite_output_dir \
-    --save_steps 30000 \
-    --save_total_limit 2
-    # --tokenizer_name lcquad_tokenizer \
+    --save_steps 10000 \
+    --save_total_limit 2 \
+    --report_to wandb \
+    --run_name ${run_name}
+    
