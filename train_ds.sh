e2991b9cd65d6f69ce385da5da5ef90c86ce1f89
Mengshi Ma <makevin23@gmail.com>
2023-06-27 12:58:28 +0200

    update train_ds template

--- train_ds.sh
+++ train_ds.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-dataset_name=""
+run_name=""
 model_name="google/mt5-xl"
-output_dir="fine-tuned_models/${dataset_name}"
-run_name="${model_name}-${dataset_name}"
+output_dir="fine-tuned_models/${run_name}"
 train_file=""
 validation_file=""
 
@@ -11,9 +10,9 @@ deepspeed --num_gpus=1 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
     --model_name_or_path ${model_name} \
     --do_train \
+    --train_file ${train_file} \
     --do_eval \
     --eval_steps 1500 \
-    --train_file ${train_file} \
     --validation_file ${validation_file} \
     --output_dir ${output_dir} \
     --num_train_epochs 15 \
