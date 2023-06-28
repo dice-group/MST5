c12b77fcd7e326e703e83e0e97b163b190ddeaba
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 14:16:11 +0200

    script to convert lcquad1 to csv

---
+++ code/lcquad1_to_csv.py
@@ -0,0 +1,25 @@
+import argparse
+from utils.data_io import read_json
+from dataset.LCquad1 import LCquad1
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="A program to extract lcqald questions and queries to csv dataset")
+
+    parser.add_argument("-i", "--input", type=str, help="name of input file")
+    parser.add_argument("-o", "--output", type=str,
+                        help="name of output file", default="lcqald.csv")
+    parser.add_argument('--linguistic_context', default=False, type=bool,
+                        help='With or without linguistic context in question string')
+    parser.add_argument('--entity_knowledge', default=False, type=bool,
+                        help='With or without entity knowledge in question string')
+
+    args = parser.parse_args()
+
+    lcquad_file = read_json(args.input)
+    lcquad_dataset = LCquad1(lcquad_file)
+    lcquad_dataset.export_csv(args.output)
+
+if __name__ == "__main__":
+    main()
