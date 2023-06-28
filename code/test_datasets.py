ed91e8c277a375c784d65639e37899c3506a2013
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 13:35:42 +0200

    add test

--- code/test_datasets.py
+++ code/test_datasets.py
@@ -1,5 +1,5 @@
 import unittest
-from dataset.LCquad1 import LCquad1Entry
+from dataset.LCquad1 import LCquad1, LCquad1Entry
 from components.Language import Language
 from components.Knowledge_graph import Knowledge_graph
 from components.Query import Query
@@ -38,6 +38,35 @@ class LCquad1Test(unittest.TestCase):
         expected_question = Question("How many movies did Stanley Kubrick direct?", Language.en)
         self.assertEqual(question.question_string, expected_question.question_string)
         self.assertEqual(question.language, expected_question.language)
+
+class Test_LCquad1(unittest.TestCase):
+     def setUp(self) -> None:
+        self.entries = [
+        {
+                "_id": "1501", 
+                "corrected_question": "How many movies did Stanley Kubrick direct?", 
+                "intermediary_question": "How many <movies> are there whose <director> is <Stanley Kubrick>?", 
+                "sparql_query": "SELECT DISTINCT COUNT(?uri) WHERE {?uri <http://dbpedia.org/ontology/director> <http://dbpedia.org/resource/Stanley_Kubrick>  . }", 
+                "sparql_template_id": 101
+        }, 
+        {
+                "_id": "2586", 
+                "corrected_question": "Which city's foundeer is John Forbes?", 
+                "intermediary_question": "What <city>'s <founded by> is <John Forbes (British Army officer)>", 
+                "sparql_query": "SELECT DISTINCT ?uri WHERE {?uri <http://dbpedia.org/ontology/founder> <http://dbpedia.org/resource/John_Forbes_(British_Army_officer)>  . ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/City>}", 
+                "sparql_template_id": 301
+        }, 
+        {
+                "_id": "2653", 
+                "corrected_question": "What is the river whose mouth is in deadsea?", 
+                "intermediary_question": "What is the <river> whose <river mouth> is <Dead Sea>?", 
+                "sparql_query": "SELECT DISTINCT ?uri WHERE {?uri <http://dbpedia.org/ontology/riverMouth> <http://dbpedia.org/resource/Dead_Sea>  . ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/River>}", 
+                "sparql_template_id": 301
+        } ]
+          
+     def test_lcquad_entries(self):
+        lcquad = LCquad1(self.entries)
+        self.assertEqual(lcquad.entries[1].question.question_string, "Which city's foundeer is John Forbes?")
 
 
 if __name__ == '__main__':
