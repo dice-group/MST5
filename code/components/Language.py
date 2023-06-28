7865ac71a60c32a65ce2eadc64bc94051e4b1767
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 12:39:10 +0200

    add language enum class

---
+++ code/components/Language.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Language(Enum):
+    en = 'en'
+    de = 'de'
+    ru = 'ru'
+    fr = 'fr'
+    lt = 'lt'
+    zh = 'zh'
+    ja = 'ja'
+    uk = 'uk'
+    be = 'be'
+    ba = 'ba'
