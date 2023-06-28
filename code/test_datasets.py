552ffa5b0d1221482ba66c9b352b4474575084ab
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 13:18:17 +0200

    add test

--- code/test_datasets.py
+++ code/test_datasets.py
@@ -3,6 +3,7 @@ from dataset.LCquad1 import LCquad1Entry
 from components.Language import Language
 from components.Knowledge_graph import Knowledge_graph
 from components.Query import Query
+from components.Question import Question
 
 class LCquad1Test(unittest.TestCase):
     def test_parse_lcquad_entry(self):		
@@ -28,13 +29,27 @@ class LCquad1Test(unittest.TestCase):
                 "sparql_template_id": 101}
 
         entry = LCquad1Entry(input_entry)
-        query = entry.get_query_from_input_entry(input_entry)
+        query: Query = entry.get_query_from_input_entry(input_entry)
 
         expected_query = Query("SELECT DISTINCT COUNT(?uri) WHERE {?uri <http://dbpedia.org/ontology/director> <http://dbpedia.org/resource/Stanley_Kubrick>  . }", Knowledge_graph.DBpedia)
 
 
         self.assertEqual(query.sparql, expected_query.sparql)
         self.assertEqual(query.knowledge_graph, expected_query.knowledge_graph)
+
+    def test_return_question(self):
+        input_entry = {"_id": "1501", 
+                "corrected_question": "How many movies did Stanley Kubrick direct?", 
+                "intermediary_question": "How many <movies> are there whose <director> is <Stanley Kubrick>?", 
+                "sparql_query": "SELECT DISTINCT COUNT(?uri) WHERE {?uri <http://dbpedia.org/ontology/director> <http://dbpedia.org/resource/Stanley_Kubrick>  . }", 
+                "sparql_template_id": 101}
+
+        entry = LCquad1Entry(input_entry)
+        question: Question = entry.get_question_from_input_query(input_entry)
+        
+        expected_question = Question("How many movies did Stanley Kubrick direct?", Language.en)
+        self.assertEqual(question.question_string, expected_question.question_string)
+        self.assertEqual(question.language, expected_question.language)
 
 
 if __name__ == '__main__':
