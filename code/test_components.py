47eee059b34385fc392a577c2f6beb8c5827acb9
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 15:37:14 +0200

    add test for question string length

--- code/test_components.py
+++ code/test_components.py
@@ -9,7 +9,11 @@ class Test_Language(unittest.TestCase):
         Language.get_spacy_nlp(Language.en)
 
 class Test_Question(unittest.TestCase):
-    pass
+    def test_input_length(self):
+        question_string = "What is the common affiliation of the Monroe Carell Jr. Children's Hospital at Vanderbilt and alma mater of the Duncan U. Fletcher? <pad> PRON AUX DET ADJ NOUN ADP DET PROPN PROPN PROPN PROPN PART PROPN ADP PROPN CCONJ NOUN NOUN ADP DET PROPN PROPN PROPN PUNCT <pad> attr ROOT det amod nsubj prep det compound compound compound poss case pobj prep pobj cc compound conj prep det compound compound pobj punct <pad> 2 1 3 3 2 3 6 7 7 6 5 6 4 5 6 7 6 5 6 8 8 8 7 2 <pad> dbr_Monroe_Carell_Jr dbr_Duncan_U"
+        question_string_split = question_string.split(" ")
+        # self.assertEqual(question_string_split, "?")
+        self.assertLessEqual(len(question_string_split), 128)
 
 class Test_Query(unittest.TestCase):
     def setUp(self) -> None:
