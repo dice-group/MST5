1114537e2d7826df78d595e70ef9b40cfa5c0f14
Mengshi Ma <mengshi.ma@klosebrothers.de>
2023-06-28 15:27:52 +0200

    add spacy nlp to language

--- code/components/Language.py
+++ code/components/Language.py
@@ -1,4 +1,28 @@
 from enum import Enum
+import spacy
+
+nlp_en = spacy.load("en_core_web_sm")
+nlp_zh = spacy.load("zh_core_web_sm")
+nlp_de = spacy.load("de_core_news_sm")
+nlp_fr = spacy.load("fr_core_news_sm")
+nlp_ja = spacy.load("ja_core_news_sm")
+nlp_lt = spacy.load("lt_core_news_sm")
+nlp_ru = spacy.load("ru_core_news_sm")
+nlp_uk = spacy.load("uk_core_news_sm")
+
+nlp_dict = {
+    "en": nlp_en,
+    "zh": nlp_zh,
+    "de": nlp_de,
+    "fr": nlp_fr,
+    "ja": nlp_ja,
+    "lt": nlp_lt,
+    "uk": nlp_uk,
+    "ru": nlp_ru,
+    "ba": nlp_ru,
+    "be": nlp_ru
+}
+
 
 class Language(Enum):
     en = 'en'
@@ -17,3 +41,10 @@ class Language(Enum):
             return Language.__members__[value]
         except KeyError:
             raise ValueError(f"Invalid language: {value}")
+    
+    def get_spacy_nlp(language):
+        try:
+            return nlp_dict[language.value]
+        except KeyError:
+            raise ValueError(f"No spacy nlp for {language}")
+    
