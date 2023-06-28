5a0d2783e41d067a075e46fff031885e6077f48c
Mengshi Ma <makevin23@gmail.com>
2023-06-29 00:19:31 +0200

    make test green

--- code/dataset/Sgpt.py
+++ code/dataset/Sgpt.py
@@ -67,7 +67,7 @@ class Sgpt_pred(Dataset):
 
 class Sgpt_entry(Entry):
     def __init__(self, entry, knowledge_graph) -> None:
-        self.ref_query: str = None
+        self.ref_query: str = super().build_query(entry["ground_truth_sparql"], knowledge_graph)
         self.pred_query: str = super().build_query(entry["predicted_sparql"], knowledge_graph)
 
     def build_pred_query(self, pred_sparql, knowledge_graph):
