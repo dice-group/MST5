0ebfd58a847e48f6b1aa7ed39222f86a75b7bd33
Mengshi Ma <makevin23@gmail.com>
2023-06-30 10:25:20 +0200

    add test

--- code/test_components.py
+++ code/test_components.py
@@ -10,6 +10,10 @@ class Test_Language(unittest.TestCase):
         Language.get_spacy_nlp(Language.en)
 
 class Test_Question(unittest.TestCase):
+    def setUp(self) -> None:
+        self.question = Question("Ist Hawaii der Geburtsort von Trump?", Language.de)
+        return super().setUp()
+
     def test_input_length(self):
         question_string = "What is the common affiliation of the Monroe Carell Jr. Children's Hospital at Vanderbilt and alma mater of the Duncan U. Fletcher? <pad> PRON AUX DET ADJ NOUN ADP DET PROPN PROPN PROPN PROPN PART PROPN ADP PROPN CCONJ NOUN NOUN ADP DET PROPN PROPN PROPN PUNCT <pad> attr ROOT det amod nsubj prep det compound compound compound poss case pobj prep pobj cc compound conj prep det compound compound pobj punct <pad> 2 1 3 3 2 3 6 7 7 6 5 6 4 5 6 7 6 5 6 8 8 8 7 2 <pad> dbr_Monroe_Carell_Jr dbr_Duncan_U"
         question_string_split = question_string.split(" ")
@@ -20,9 +24,17 @@ class Test_Question(unittest.TestCase):
         pass
 
     def test_detect_entity_with_davlan(self):
-        question = Question("Ist Hawaii der Geburtsort von Trump?", Language.de)
-        response = question.send_entity_detection_request("davlan_ner")
+        self.question = Question("Ist Hawaii der Geburtsort von Trump?", Language.de)
+        response = self.question.send_entity_detection_request("davlan_ner")
         self.assertTrue("ent_mentions" in response)
+        print(response)
+
+    def test_process_ner_response(self):
+        response = '''{"components":"davlan_ner, mgenre_el","ent_mentions":[{"end":10,"link":"Q782","link_candidates":[["Hawaii","de","Q782"]],"start":4,"surface_form":"Hawaii"},{"end":35,"link":"Q22686","link_candidates":[["Donald Trump","de","Q22686"]],"start":30,"surface_form":"Trump"}],"kb":"wd","lang":"de","placeholder":"00","replace_before":false,"text":"Ist Hawaii der Geburtsort von Trump?"}'''
+        entities = self.question.process_ner_response(response)
+        self.assertEqual(entities["Hawaii"], "Q782")
+        self.assertEqual(entities["Donald Trump"], "Q22686")
+        
 
 class Test_Query(unittest.TestCase):
     def setUp(self) -> None:
@@ -57,3 +69,7 @@ class Test_Query(unittest.TestCase):
         wikidata_query = Query("SELECT DISTINCT ?o1 WHERE { <http://www.wikidata.org/entity/Q23337>  <http://www.wikidata.org/prop/direct/P421>  ?o1 .  }", Knowledge_graph.Wikidata)
         answer = wikidata_query.get_answer()
         self.assertTrue(answer["results"]["bindings"])
+
+
+if __name__ == '__main__':
+    unittest.main()
