11d0fd557f84a80e5d0ee3a0c162d4d15cddd828
Mengshi Ma <makevin23@gmail.com>
2023-06-30 17:46:53 +0200

    get eval script to normal

--- eval.sh
+++ eval.sh
@@ -4,7 +4,7 @@ model="mT5-lcquad-ling-entity"
 pred_path=pred_files/${model}
 test_dataset="datasets/qald9plus/wikidata/qald_9_plus_test_wikidata_new.json"
 knowledge_graph="Wikidata"
-languages=("en" "de" "ru" "fr" "lt" "ba" "be" "uk" "zh" "ja")
+languages=("en" "de" "ru" "fr" "lt" "ba" "be" "uk" "zh" "ja" "es")
 
 include_linguistic_context=true
 include_entity_knowledge=true
@@ -21,18 +21,18 @@ else
   entity_knowledge="--no-entity_knowledge"
 fi
 
-# for lang in "${languages[@]}"
-# do
-#     echo "Generating predicted qald file for ${lang}"
-#     python code/pred_and_build_qald.py \
-#         --model fine-tuned_models/${model} \
-#         -t ${test_dataset} \
-#         --knowledge_graph ${knowledge_graph} \
-#         -o ${pred_path}/${lang}.json \
-#         -l ${lang} \
-#         ${linguistic_context} \
-#         ${entity_knowledge}
-# done
+for lang in "${languages[@]}"
+do
+    echo "Generating predicted qald file for ${lang}"
+    python code/pred_and_build_qald.py \
+        --model fine-tuned_models/${model} \
+        -t ${test_dataset} \
+        --knowledge_graph ${knowledge_graph} \
+        -o ${pred_path}/${lang}.json \
+        -l ${lang} \
+        ${linguistic_context} \
+        ${entity_knowledge}
+done
 
 echo "Start running GERBIL experiment"
 python code/gerbil_eval.py \
