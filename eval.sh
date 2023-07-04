908b5a8aa5238613d676b19b1bed6b1170b0513c
Mengshi Ma <makevin23@gmail.com>
2023-07-04 09:45:27 +0200

    eval script for exp12

--- eval.sh
+++ eval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-model="exp12-fine-tune"
+model="exp8-mt5xl-lcquad-lc-ek"
 pred_path=pred_files/${model}
-test_dataset="datasets/qald9plus/dbpedia/qald_9_plus_test_dbpedia-new.json"
-knowledge_graph="DBpedia"
+test_dataset="datasets/qald9plus/wikidata/qald_9_plus_test_wikidata_new.json"
+knowledge_graph="Wikidata"
 languages=("en" "de" "ru" "fr" "lt" "ba" "be" "uk" "zh" "ja" "es")
 
 include_linguistic_context=true
-include_entity_knowledge=false
+include_entity_knowledge=true
 
 if [ "$include_linguistic_context" = true ]; then
   linguistic_context="--linguistic_context"
