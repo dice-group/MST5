e3270ead338ddf9ac9b472d6abbb641a2169f460
Mengshi Ma <makevin23@gmail.com>
2023-07-05 16:56:08 +0200

    adapt test

--- code/test_components.py
+++ code/test_components.py
@@ -99,16 +99,21 @@ class Test_Question(unittest.TestCase):
         self.assertEqual(en_uri, "dbr_Harry_Potter")
 
     def test_pad_question_string(self):
-        question_string: str = self.question.pad_to_64()
+        question_string: str = self.question.pad_to_64(length=64)
         self.assertTrue("<pad>" in question_string)
         self.assertTrue(len(question_string.split(" ")), 64)
 
     def test_pad_pos(self):
         pos_tags = "PRON AUX DET ADJ NOUN ADP NOUN ADP PROPN PROPN PROPN"
-        padded_pos_tags = self.question.pad_to_64(pos_tags)
+        padded_pos_tags = self.question.pad_to_length(pos_tags, 64)
         self.assertTrue("<pad>" in padded_pos_tags)
         self.assertTrue(len(padded_pos_tags.split(" ")), 64)
 
+    def test_length_after_padding(self):
+        question_string_with_lc = self.question.get_question_string_with_lingtuistic_context()
+        self.assertEqual(len(question_string_with_lc.split(" ")), 256)
+        self.assertTrue("ROOT" in question_string_with_lc)
+        print(question_string_with_lc)
 
 
 
