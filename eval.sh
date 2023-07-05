4a10f3db2433aa45bcfd49fa13807f2dc3d15cba
Mengshi Ma <makevin23@gmail.com>
2023-07-05 13:55:12 +0200

    eval script for exp8-lc

--- eval.sh
+++ eval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-model="exp8-mt5xl-lcquad-lc-ek"
+model="exp8-lc"
 pred_path=pred_files/${model}
 test_dataset="datasets/qald9plus/wikidata/qald_9_plus_test_wikidata_new.json"
 knowledge_graph="Wikidata"
 languages=("en" "de" "ru" "fr" "lt" "ba" "be" "uk" "zh" "ja" "es")
 
 include_linguistic_context=true
-include_entity_knowledge=true
+include_entity_knowledge=false
 
 if [ "$include_linguistic_context" = true ]; then
   linguistic_context="--linguistic_context"
