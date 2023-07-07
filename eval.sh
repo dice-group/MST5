ea4e33ea3fa61ca799c1f2f1bde5226213375d1c
Mengshi Ma <makevin23@gmail.com>
2023-07-07 09:04:48 +0200

    update evaluation script

--- eval.sh
+++ eval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-model="exp8-lc"
+model="exp13-mt5xl-lcquad-lc-ek-pad-dbp"
 pred_path=pred_files/${model}
-test_dataset="datasets/qald9plus/wikidata/qald_9_plus_test_wikidata_new.json"
-knowledge_graph="Wikidata"
+test_dataset="datasets/qald9plus/dbpedia/qald_9_plus_test_dbpedia-new.json"
+knowledge_graph="DBpedia"
 languages=("en" "de" "ru" "fr" "lt" "ba" "be" "uk" "zh" "ja" "es")
 
 include_linguistic_context=true
-include_entity_knowledge=false
+include_entity_knowledge=true
 
 if [ "$include_linguistic_context" = true ]; then
   linguistic_context="--linguistic_context"
