968eec4ab3a7392020180b99d723e66d2bc545c5
Mengshi Ma <makevin23@gmail.com>
2023-07-07 09:05:00 +0200

    fine-tune script

--- train_ds.sh
+++ train_ds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-run_name="lcquad-lc-ek-pad-dbp"
-model_name="google/mt5-xl"
-output_dir="pre-trained_models/${run_name}"
-train_file="datasets/lcquad1/train-data.csv"
+run_name="exp13-mt5xl-lcquad-lc-ek-pad"
+model_name="pre-trained_models/lcquad-lc-ek-pad-dbp"
+output_dir="fine-tuned_models/${run_name}"
+train_file="datasets/qald9plus/dbpedia/qald_9_plus-train_dbpedia.csv"
 
 deepspeed --include=localhost:1 --master_port 61000 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
@@ -11,10 +11,10 @@ deepspeed --include=localhost:1 --master_port 61000 code/train_new.py \
     --do_train \
     --train_file ${train_file} \
     --output_dir ${output_dir} \
-    --num_train_epochs 32 \
+    --num_train_epochs 15 \
     --per_device_train_batch_size=16 \
     --overwrite_output_dir \
-    --save_steps 1000 \
+    --save_steps 600 \
     --save_total_limit 2 \
     --report_to wandb \
     --run_name ${run_name}\
