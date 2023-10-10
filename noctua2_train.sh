1bc8eb8bb9953e6d260de9f67099a7841cdc4914
Nikit Srivastava <nikit91@gmail.com>
2023-10-10 18:30:13 +0200

    adding qald ablation instructions

--- noctua2_train.sh
+++ noctua2_train.sh
@@ -3,13 +3,11 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:a100:1
-#SBATCH --mem 220G
+#SBATCH --mem 120G
 ###SBATCH --partition=dgx
 ###SBATCH --qos=devel
 #SBATCH -t 40:00:00
 #SBATCH -o "train_logs/lcquad2_ft_slurm-%j.out"
-
-# Sample usage: sbatch noctua2_train.sh "lc-ent" 60000
 
 module load lib/NCCL/2.12.12-GCCcore-11.3.0-CUDA-11.7.0
 module load ai/PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
@@ -20,24 +18,29 @@ source mst5-venv/bin/activate
 export HF_DATASETS_CACHE="/scratch/hpc-prf-lola/nikit/.cache/huggingface"
 export HUGGINGFACE_HUB_CACHE="/scratch/hpc-prf-lola/nikit/.cache/huggingface"
 
-TRAIN_POSTFIX=$1
-PORT=$2
+PORT=$1
+MODEL_NAME=$2
+TRAIN_FILE=$3
+OUTPUT_DIR=$4
+RUN_NAME=$5
+TRAIN_EPOCHS=$6
+SAVE_STEPS=$7
 
-echo Starting training for: $TRAIN_POSTFIX
+
 
 deepspeed --include=localhost:0 --master_port $PORT code/train_new.py \
     --deepspeed deepspeed/ds_config_zero2.json \
-    --model_name_or_path google/mt5-xl \
+    --model_name_or_path $MODEL_NAME \
     --do_train \
-    --train_file datasets/lcquad2/train-$TRAIN_POSTFIX.csv \
-    --output_dir fine-tuned_models/lcquad2-finetune-$TRAIN_POSTFIX \
-    --num_train_epochs 15 \
+    --train_file $TRAIN_FILE \
+    --output_dir $OUTPUT_DIR \
+    --num_train_epochs $TRAIN_EPOCHS \
     --per_device_train_batch_size=16 \
     --overwrite_output_dir \
-    --save_steps 1000 \
+    --save_steps $SAVE_STEPS \
     --save_total_limit 2 \
     --report_to wandb \
-    --run_name lcquad2-finetune-$TRAIN_POSTFIX \
+    --run_name $RUN_NAME \
     --logging_steps 10 \
     --tf32 1 \
     --fp16 0 \
