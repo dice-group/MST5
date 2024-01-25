6e29a312725582e6ebcbe2d37985324e8a6b4e65
Nikit Srivastava <nikit91@gmail.com>
2024-01-25 20:26:07 +0100

    fixing noctua2 setup

--- noctua2/noctua2_prepare_data.sh
+++ noctua2/noctua2_prepare_data.sh
@@ -2,8 +2,8 @@
 #SBATCH -J "mst5-data-prep"
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-###SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=64
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem 2G
 ###SBATCH --partition=dgx
 ###SBATCH --qos=devel
@@ -12,10 +12,9 @@
 
 # Sample usage: sbatch noctua2/noctua2_prepare_data.sh
 
-module load ai/PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 module load vis/torchvision/0.13.1-foss-2022a-CUDA-11.7.0
 
 source mst5-venv/bin/activate
 
 # pass the parameters to train script
-bash prepare_data.sh "$@"
+bash prepare_data.sh "$@" 2>&1
---
+++ noctua2/setup_environment.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -J "MST5: dependencies installation"
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH -t 01:00:00
+
+# Sample usage: sbatch noctua2/setup_environment.sh .
+set -eu
+
+module load vis/torchvision/0.13.1-foss-2022a-CUDA-11.7.0
+
+# Check if the first argument is set, if not assign the current directory path
+MST5_WS=$(pwd)
+echo "Current MST5 workspace: ${MST5_WS}"
+VENVPATH="${1:-$(pwd)}"
+echo "Path to install virtual environment at: ${VENVPATH}"
+
+mkdir -p $VENVPATH
+
+cd $VENVPATH
+
+python -m venv ./mst5-venv
+
+source mst5-venv/bin/activate
+
+pip3 install --upgrade pip
+
+pip3 install -r requirements.txt
+
+pip install -U pip setuptools wheel
+pip install -U spacy
+python -m spacy download zh_core_web_sm
+python -m spacy download en_core_web_sm
+python -m spacy download fr_core_news_sm
+python -m spacy download de_core_news_sm
+python -m spacy download ja_core_news_sm
+python -m spacy download lt_core_news_sm
+python -m spacy download ru_core_news_sm
+python -m spacy download es_core_news_sm
+python -m spacy download uk_core_news_sm
+
+echo "Dependencies installation finished!"
