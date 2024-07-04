6f72b99556321e5105a66e0e9407dc6e21b1663c
denkv <denkv@users.noreply.github.com>
2024-07-04 12:10:14 +0200

    Do not specify a CUDA device

--- code/components/Text_Generator.py
+++ code/components/Text_Generator.py
@@ -5,7 +5,8 @@ class Text_Generator:
         self.generator = self.init_generator(model_path)
 
     def init_generator(self, model_path):
-        return pipeline("text2text-generation", model=model_path, max_length=256, device=0)
+        # add device= to specify a GPU or -1 to run on CPU
+        return pipeline("text2text-generation", model=model_path, max_length=256)
 
     def predict_sparql(self, question_string):
         return self.generator(question_string)[0]['generated_text']
